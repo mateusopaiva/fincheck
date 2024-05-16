594331e97f107abd622cdd3ee41533644e450e0b
Mateus Paiva <mateusopaiva@gmail.com>
2024-05-16 18:46:14 -0300

    [FRONT] Dashboard layout e slider de bank accounts

---
+++ frontend/src/view/pages/Dashboard/components/Transactions.tsx
@@ -0,0 +1,7 @@
+export function Transactions() {
+  return (
+    <div className="bg-gray-100 rounded-2xl w-full h-full p-10">
+      Transactions
+    </div>
+  );
+}
--- frontend/src/view/pages/Dashboard/index.tsx
+++ frontend/src/view/pages/Dashboard/index.tsx
@@ -1,16 +1,24 @@
-import { useAuth } from "../../../app/hooks/useAuth";
-import { Button } from "../../components/Button";
+import { Logo } from "../../components/Logo";
+import { UserMenu } from "../../components/UserMenu";
+import { Accounts } from "./components/Accounts";
+import { Transactions } from "./components/Transactions";
 
 export function Dashboard() {
-  const { signout } = useAuth()
-
   return (
-    <div>
-      <h1>Dashboard Page</h1>
+    <div className="h-full w-full p-4 md:px-8 nd:pb-8 md:pt-6 flex flex-col gap-4">
+      <header className="h-12 flex items-center justify-between">
+        <Logo className="h-6 text-teal-900"/>
+        <UserMenu />
+      </header>
 
-      <Button onClick={signout}>
-        Sair
-      </Button>
+      <main className="flex-1 flex flex-col md:flex-row gap-4">
+        <div className="w-full md:w-1/2 ">
+          <Accounts />
+        </div>
+        <div className="w-full md:w-1/2 ">
+          <Transactions />
+        </div>
+      </main>
     </div>
   );
 }
